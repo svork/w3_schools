ed5cc3fe5a87e1277ac8ad8b3dcc5f0636ef1383
Rodrigo Costa <svork@users.noreply.github.com>
2018-03-01 11:03:24 -0300

    Even more JS Array methods

---
+++ javascript/12_js_array.js
@@ -0,0 +1,49 @@
+// Variables
+var i = 0;
+var people = ["Lais", "Mariana", "Daniela", "Hillory", "Juliana", "Luiza", "Aretuza"];
+
+// This function will iterate over an array, using a simple FOR statement
+function proximo(){
+  // This will go over each element of the array, showing the value and the index
+  for ( i; i < people.length; ) {
+    document.getElementById("lbl_item").innerHTML = i;
+    document.getElementById("lbl_name").innerHTML = people[i];
+    console.log(i);
+    i++;
+    break;
+  }
+}
+
+// This function will sort the array
+function arrumar(){
+  document.getElementById("lbl_array").innerHTML = people.toString();
+  document.getElementById("lbl_arrumado").innerHTML = people.sort();
+}
+
+// This function adds an element to the end of the array
+function adicionar(){
+  var name = document.getElementById("txt_name").value;
+  people.push(name);
+  document.getElementById("lbl_new_name").innerHTML = people.toString();
+}
+
+// This function removes an element from the end of the array
+function remover(){
+  var removida = people.pop();
+  alert("A pessoa " + removida + " foi removida");
+  document.getElementById("lbl_new_name").innerHTML = people.toString();
+}
+
+// This function removes an element from the beginnig of the array
+function remover_inicio(){
+  var removida = people.shift();
+  alert("A pessoa " + removida + " foi removida");
+  document.getElementById("lbl_new_name").innerHTML = people.toString();
+}
+
+// This function adds an element to the beginning of the array
+function adicionar_inicio(){
+  var name = document.getElementById("txt_name").value;
+  people.unshift(name);
+  document.getElementById("lbl_new_name").innerHTML = people.toString();
+}
